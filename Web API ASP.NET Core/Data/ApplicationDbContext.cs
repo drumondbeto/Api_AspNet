098e3d6ba68a02841219bfc43fe0d251ab4ffcff
Humberto Drumond <humbertowdrumond@gmail.com>
2021-11-19 12:31:25 -0300

    Cria a Model da classe Fornecedor

---
+++ Web API ASP.NET Core/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_ASP.NET_Core.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+    }
+}
---
+++ Web API ASP.NET Core/Model/Fornecedor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_API_ASP.NET_Core.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [StringLength(3, ErrorMessage = "O campo {0} deve ter entre {1} e {2} caracteres.", MinimumLength = 80)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(14, ErrorMessage = "O campo {0} deve ter entre {1} e {2} caracteres.", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        [Required]
+        public bool Ativo { get; set; }
+    }
+}
