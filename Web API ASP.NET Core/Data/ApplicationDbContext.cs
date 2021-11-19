a189e8f3b95b56da47ccb096a52b74891b530f90
Humberto Drumond <humbertowdrumond@gmail.com>
2021-11-19 12:37:35 -0300

    Configura a conexão com o banco de dados PostgreSQL

--- Web API ASP.NET Core/Data/ApplicationDbContext.cs
+++ Web API ASP.NET Core/Data/ApplicationDbContext.cs
@@ -8,6 +8,9 @@ namespace Web_API_ASP.NET_Core.Data
 {
     public class ApplicationDbContext : DbContext
     {
-
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Server=localhost;Port=5432;Database=WebApiAspNetCoreDatabase;User ID=postgres;Password=123456;");
+        }
     }
 }
